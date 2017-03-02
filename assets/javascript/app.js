06370d836e9169f2a5c72dc8f201c677f4a11b69
Caryn Carter <caryn.carter@northwestern.com>
2017-03-01 18:03:00 -0600

    added psedocode and beginning code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,29 @@
 /* Pseudo code: 
 
-Need an object with the questions
+Object with the questions
 var questions : {
 	
 }
 
-Need the questions to have attributes (1)name, (2) question, (3) possible 
+Questions to have attributes (1)name, (2) question, (3) possible 
 answers ARRAY, (4) correct answer, which is equal to one of the answers, 
 (5) a number to increment as the number of times the question is used goes up.
 
-Can the questions object be an array?  Need math.floor/math.random to pick 
+Need math.floor/math.random to pick 
 the questions. 
 
 Need 10 questions.  Should present 5 and then start over the game. 
 
+Function: startGame function onClick of the Start Game button. 
+	black screen resets with new question
+	values (1) total correct (2) incorrect --- both clear and are not visible
+	timer resets
+
+Function: displayQuestion -- on answer display will wait 3 seconds and then show new question
+
 */
+$(document).ready () {
+
 
 var questions = {
 	q1: {
@@ -22,7 +31,7 @@ var questions = {
 		questionPicture: null,
 		answers: ["The Hermitage", "The British Museum", "The Prado", "The Louvre"],
 		useCount: "",
-		correctAnswer: answers[3],
+		correctAnswer: questions.q1.answers[3],
 		answerExp: "The Louvre Museum is the largest museum in the world.",
 		answerPhoto: null
 	}
@@ -81,16 +90,16 @@ var questions = {
 		answerPhoto: "..images/venusdemilo.jpg"
 	}
 	q8: {
-		question: "These Lamassu figures in the Louvre from the citadel of Sargon II are from which ancient near eastern civilization?",
-		quesitonPicture: , 
+		question: "These Lamassu figures from the citadel of Sargon II are from which ancient near eastern civilization?",
+		quesitonPicture: "..images/lamassu.jpg", 
 		answers: ["Egyptian","Babylonian", "Assyrian", "Greek"],
 		useCount: "",
 		correctAnswer: answers[2],
 		answerExp: "Meant to protect the kingdom from enemy visitors, the Lamassu were imposing symbols of the Assyrian Empire (934-609 B.C.E.).",
-		answerPhoto: "..images/lamassu.jpg"
+		answerPhoto: null
 	}
 	q9: {
-		question: "True or False: The Winged Victory of Samothrace in the Louvre was initially discovered with only one wing.",
+		question: "True or False: The Winged Victory of Samothrace was initially discovered with only one wing.",
 		questionPicture: ,
 		answers: ["True," "False"],
 		useCount: "",
@@ -99,14 +108,80 @@ var questions = {
 		answerPhoto: null
 	}
 	q10: {
-		question: "Liberty Leading the People, in the Louvre, painted by Eugene Delacroix, has come to symbolize which Broadway Musical?",
+		question: "Liberty Leading the People, painted by Eugene Delacroix, has been associated which Broadway Musical?",
 		questionPicture: ,
 		answers: ["Hamilton", "Evita", "Les Miserables", "Man of La Mancha"],
 		useCount: "",
 		correctAnswer: answers[2],
-		answerExp: "Both Liberty Leading the People and Les Miserables, originally a book by Victor Hugo that became a musical, depict the French Revolution.",
+		answerExp: "Both Liberty Leading the People and Les Miserables, originally a novel by Victor Hugo that became a musical, depict the French Revolution.",
 		answerPhoto: , 
 	}
 
 }
+
+var gameState : {
+	totalCorrect: 0,
+	totalIncorrect: 0,
+	currentQuestion: "",
+	displayQuestionCount: 0,
+	timer: 0,
+}
+
+function dipslayQuestion () {
+	// grabs math.floor/math.random question and displays it in the black screen
+	// if the useCount === 0, display question
+	// waits 10 seconds, then moves to next question
+	// displayQuestionCount ++; 
+
+}
+
+function processAnswer () {
+	// takes in user answer and then compares it to the right answer
+	showAnswer (); 
+
+}
+
+
+function showAnswer () {
+	// shows user the right answer if they got it right or wrong
+	// waits 5 seconds before moving on
+	// if statement that if the displayQuestionCount > 4, show game over and final counts 
+	gameOver (); 
+}
+
+function gameOver () {
+	//shows total counts, totalCorrect and totalIncorrect, says "press start to play again"
+}
+
+function count () {
+	gameState.timer++;
+	var counter = setInterval(gameState.timer,1000); 
+	$("#timeDisplay").html(gameState.timer);
+}
+
+function startGame () {
+	gameState.totalCorrect = 0;
+	gameState.totalIncorrect = 0;
+	gameState.timer =0; 
+	count(); 
+	displayQuestion (); 
+}
+
+
+
+	$("#start").click(startGame); 
+
+	});
+
+	$("....").on("Click", function () {
+		processAnswer (); 
+
+
+	});
+
+
+
+
+	
+});
 
