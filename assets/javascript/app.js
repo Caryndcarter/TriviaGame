d9fb181e836fba41bf0d0f09543fc14ed39fcfc1
Caryn Carter <caryn.carter@northwestern.com>
2017-03-02 22:14:37 -0600

    got question to be chosen and displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,11 +27,15 @@ $(document).ready(function() {
 
 	$(".triviaboard").hide();
 
-console.log("hello!");
+	$(".start").click(function () {
+		$(".triviaboard").show();
+		chooseQuestion(); 
+	});	
 
-var questions = {
-	q1: {
-		question: "What is the name of the world-class museum pictured in the background here?",
+var questions = [
+	{
+		question: "What is the name of the world-class museum pictured in the background?",
+		questionNumber: 1,
 		questionPicture: null,
 		answers: ["The Hermitage", "The British Museum", "The Prado", "The Louvre"],
 		useCount: 0,
@@ -39,8 +43,9 @@ var questions = {
 		answerExp: "The Louvre Museum is the largest museum in the world.",
 		answerPhoto: null
 	},
-	q2: {
+	{
 		question: "Where is the Louvre?",
+		questionNumber: 2,
 		questionPicture: null,
 		answers: ["Paris", "London", "Madrid", "Berlin"],
 		useCount: 0,
@@ -48,8 +53,9 @@ var questions = {
 		answerExp: "The Louvre is a historic landmark in the city of Paris.",
 		answerPhoto: "../images/paris_380x244.jpg"
 	},
-	q3: {
+	{
 		question: "What is the most famous painting in the Louvre?",
+		questionNumber: 3,
 		questionPicture: null, 
 		answers: ["The Starry Night", "The Scream", "The Mona Lisa", "The Last Supper"],
 		useCount: 0,
@@ -57,8 +63,9 @@ var questions = {
 		answerExp: "The Mona Lisa is a portrait of Lisa Gherardini by Leonardo da Vinci, on display in the Louvre since 1797.",
 		answerPhoto:"../images/monalisa.jpg"
 	},
-	q4: {
+	{
 		question: "The oldest foundations of the Louvre date to which century?",
+		questionNumber: 4,
 		questionPicture: null,
 		answers: ["19th", "17th", "15th", "12th"],
 		useCount: 0,
@@ -66,8 +73,9 @@ var questions = {
 		answerExp: "The Louvre was originally built as a fortress in 1190 with a moat to defend against the north.",
 		answerPhoto: null
 	},
-	q5: {
+	{
 		question: "What event triggered the Louvre becoming a museum?",
+		questionNumber: 5, 
 		questionPicture: null, 
 		answers: ["Joan of Arc led a victory against the British at Orleans", "Louis XIV moved out to Versailles", "Napoleon crowned himself Emperor", "France and the Allies defeated Germany in WWI"],
 		useCount: 0,
@@ -75,8 +83,9 @@ var questions = {
 		answerExp: "In 1682, Louis XIV left the Louvre as a palace and moved to Versailles. After the French Revolution, the first exhibits in 1793 displayed the royal collection of art.",
 		answerPhoto: null
 	},
-	q6: {
+	{
 		question: "True or False: The architect who designed the glass pyramid at the Louvre was of at least partial European heritage?",
+		questionNumber: 6,
 		questionPicture: null, 
 		answers: ["True", "False"],
 		useCount: 0,
@@ -84,8 +93,9 @@ var questions = {
 		answerExp: "I.M. Pei, a Chinese-born American architect, was chosen by the French President to re-develop the Louvre in the 1980s.  He won the Pritzker Prize, the Nobel Prize for Architecture, in 1983.",
 		answerPhoto: null
 	},
-	q7: {
+	{
 		question: "What is the most beloved sculpture in the Louvre?",
+		questionNumber: 7,
 		questionPicture: null,
 		answers: ["Venus de Milo","Winged Victory of Samothrace", "The Dying Slave by Michelangelo", "Psyche Revived by Cupid's Kiss by Antonio Canova"],
 		useCount: 0,
@@ -93,8 +103,9 @@ var questions = {
 		answerExp: "Venus de Milo is one of the most famous Greek sculptures of all time, created between 130 and 100 B.C., and is always surrounded with vistors.",
 		answerPhoto: "../images/venusdemilo.jpg"
 	},
-	q8: {
+	{
 		question: "These Lamassu figures from the citadel of Sargon II are from which ancient near eastern civilization?",
+		questionNumber: 8, 
 		quesitonPicture: "../images/lamassu.jpg", 
 		answers: ["Egyptian","Babylonian", "Assyrian", "Greek"],
 		useCount: 0,
@@ -102,8 +113,9 @@ var questions = {
 		answerExp: "Meant to protect the kingdom from enemy visitors, the Lamassu were imposing symbols of the Assyrian Empire (934-609 B.C.E.).",
 		answerPhoto: null
 	},
-	q9: {
+	{
 		question: "True or False: The Winged Victory of Samothrace was initially discovered with only one wing.",
+		questionNumber: 9, 
 		questionPicture: null,
 		answers: ["True", "False"],
 		useCount: 0,
@@ -111,8 +123,9 @@ var questions = {
 		answerExp: "Also called the Nike of Samothrace, her outstretched right wing is a symmetric, plaster version of the original left one.",
 		answerPhoto: null
 	},
-	q10: {
+	{
 		question: "Liberty Leading the People, painted by Eugene Delacroix, has been associated which Broadway Musical?",
+		questionNumber: 10,
 		questionPicture: null,
 		answers: ["Hamilton", "Evita", "Les Miserables", "Man of La Mancha"],
 		useCount: 0,
@@ -121,7 +134,7 @@ var questions = {
 		answerPhoto: "../images/libertysing.jpg" 
 	}
 
-}
+];
 
 var gameState = {
 	totalCorrect: 0,
@@ -132,8 +145,30 @@ var gameState = {
 	timer: 10,
 }
 
+function chooseQuestion () {
+	var presentedQuestion = Math.floor(Math.random() * 10 + 1);
+	console.log(presentedQuestion);
 
+	if(questions[presentedQuestion].useCount === 0) {
+		$(".question-answer").html(questions[presentedQuestion].question);
+			for (var i = 0; i < questions[presentedQuestion].answers.length; i++) {
+				$(".options").append("<div>" + questions[presentedQuestion].answers[i] + "</div>"); 
+			}
+		questions[presentedQuestion].useCount++;
 
+	} else {
+		chooseQuestion (); 
+	}
+
+	console.log(questions[presentedQuestion].useCount);
+	
+}
+
+	
+
+function resetQuestions () {
+
+}
 
 
 });
