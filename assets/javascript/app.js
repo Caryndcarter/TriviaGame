a003564fefe55f38c4b0e76a0b8a789099a002fc
Caryn Carter <caryn.carter@northwestern.com>
2017-03-04 00:01:15 -0600

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,14 +12,14 @@ answers ARRAY, (4) correct answer, which is equal to one of the answers,
 Need math.floor/math.random to pick 
 the questions. 
 
-Need 10 questions.  Should present 5 and then start over the game. 
+Need 10 questions.  Should present 6 and then start over the game. 
 
 Function: startGame function onClick of the Start Game button. 
 	black screen resets with new question
 	values (1) total correct (2) incorrect --- both clear and are not visible
 	timer resets
 
-Function: displayQuestion -- on answer display will wait 3 seconds and then show new question
+Function: displayQuestion -- on answer display will wait 8 seconds and then show new question
 
 */
 
@@ -35,7 +35,7 @@ var questions = [
 		answers: ["The Hermitage", "The British Museum", "The Prado", "The Louvre"],
 		useCount: 0,
 		correctAnswer: 3,
-		answerExp: "The Louvre Museum is the largest museum in the world.",
+		answerExp: "The Louvre Museum is the largest museum in the world.  Its pyramid is iconic.",
 		answerPhoto: ""
 	},
 	{
@@ -132,6 +132,7 @@ var questions = [
 ];
 
 var presentedQuestion = "";
+var intervalID = "";
 
 var gameState = {
 	totalCorrect: 0,
@@ -143,6 +144,9 @@ var gameState = {
 	timer: 16,
 }
 
+/* Function that lays out the screen at the beginning of the game. The board is hidden. The start
+button is available. */
+
 function beginning () {
 	$(".triviaboard").hide();
 
@@ -152,17 +156,52 @@ function beginning () {
 	});
 }
 
+/* Function that lays starts the game.  The board is refreshed (in case it's round 2).  The stats are
+refreshed (in case it's round 2).  The question is chosen. */
+
 function startGame () {	
 	refreshBoard (); 
-	chooseQuestion();
+	refreshStats (); 
+	chooseQuestion(); 
 }
 
+function refreshStats () {
+	gameState.totalIncorrect = 0; 
+	gameState.totalCorrect = 0; 
+	gameState.totalUnanswered = 0;
+	gameState.totalQuestionCount = 0;
+	gameState.timer = 16; 	
+		for (var i = 0; i < 10; i++) {
+		questions[i].useCount = 0; 
+	}
+}
+
+function refreshBoard () {
+	
+	$(".timer").html("<div></div>");
+	$(".picture").html("<div></div>");
+	$(".question-answer").html("<div></div>");
+	$(".options").html("<div></div>");
+}
+
+/* Function that chooses the question randomly and displays it and starts the timer. It figures
+out if the question has been asked before in this round and if so, it doesn't choose it. It goes
+through the process of trying to choose again, if so. 
+
+If 6 questions have already been played, it ends the game. It refreshes the board and it shows 
+the stats at game's end. */
+
 function chooseQuestion () {
+	clearTimer (); 
+	$(".timer").html("<div> 00:15 </div>");
 	presentedQuestion = Math.floor(Math.random(0 - 10) * 10);
 	console.log("presented question:" + presentedQuestion);
 
 	if (gameState.totalQuestionCount < 6) {
 		if(questions[presentedQuestion].useCount === 0) {
+			gameState.timer = 16;
+			startTimer ();
+
 			$(".picture").html("<img src=" + questions[presentedQuestion].questionPicture + ">");
 			$(".question-answer").html(questions[presentedQuestion].question);
 				for (var i = 0; i < questions[presentedQuestion].answers.length; i++) {
@@ -174,18 +213,13 @@ function chooseQuestion () {
 
 			questions[presentedQuestion].useCount++;
 			gameState.totalQuestionCount ++; 
-			startTimer ();
 			grabSelection ();
-				if (gameState.timer === 0) {
-					chooseQuestion (); 
-				}
 
 		} else {
 			chooseQuestion (); 
 		}
 
 	} else if (gameState.totalQuestionCount === 6) {
-		// alert ("Game Over");
 		refreshBoard (); 
 		showStats();
 	}
@@ -194,35 +228,37 @@ function chooseQuestion () {
 	console.log("total # of q's: " + gameState.totalQuestionCount);
 }
 
-
-function refreshBoard () {
-	gameState.totalQuestionCount === 0;
-	$(".picture").html("<div></div>");
-	$(".question-answer").html("<div></div>");
-	$(".options").html("<div></div>");
-	for (var i = 0; i < 10; i++) {
-		questions[i].useCount === 0; 
-	}
-}
+/* Function that shows all the stats, but then also calculates how many questions were unanswered
+because the time elapsed and no answer was selected. */
 
 function showStats () {
+	clearTimer ();
+	$(".timer").html("<div></div>");
 	$(".question-answer").html("<div><h2> Game Over </h2></div>");
 	$(".options").html("<div> Correct: " + gameState.totalCorrect + "</div>");
-	$(".options").append("<div> Incorrect: " + gameState.totalIncorrect + "</div>");
+	$(".options").append("<div> Incorrect: " + gameState.totalIncorrect + "</div>");	
+	var totalAnswered = parseInt(gameState.totalCorrect) + parseInt(gameState.totalIncorrect);
+	console.log(totalAnswered);
+
+	gameState.totalUnanswered = 6 - totalAnswered; 
+	console.log(gameState.totalUnanswered);
 	$(".options").append("<div> Unanswered: " + gameState.totalUnanswered + "</div>"); 
+	
 }
 
+/* Function that grabs the data from the choice that the user selected for the answer. */
 
 function grabSelection () {
 	$(".choices-class").click(function () {
-	// alert("I'm chosing here!");
 		gameState.selection = $(this).text(); 
 		console.log(gameState.selection);
-		clearTimer ();
 		compareAnswers (); 
 	}); 
 
 }
+
+/* Function that compares the user's answer to the right answer.  If the user is right, it adds
+to the total correct amount.  If the user is wrong, it adds to the total incorrect amount. */
 
 function compareAnswers () {
 	if (gameState.selection === questions[presentedQuestion].answers[questions[presentedQuestion].correctAnswer] ) {
@@ -235,17 +271,32 @@ function compareAnswers () {
 	showAnswer (); 
 }
 
+/* Function that shows the correct answer and runs a different timer that's shorter. */
 
 function showAnswer () {
+	clearTimer (); 
+	$(".timer").html("<div></div>");
 	$(".question-answer").html("<div><h3> Answer: </h3></div>");
 	$(".options").html("<div><h2>" + questions[presentedQuestion].answers[questions[presentedQuestion].correctAnswer] + "</h2></div>");
 	$(".options").append("<br><div>" + questions[presentedQuestion].answerExp + "</div>");
 	$(".picture").html("<img src="+ questions[presentedQuestion].answerPhoto + ">");
+	startAnswerTimer (); 
 }
+
+/* Function is the shorter timer for just the answer screen. */
+
+function startAnswerTimer () {
+	gameState.timer = 8; 
+	intervalID = setInterval(countAnswer,1000);
+}
+
+/* Function that starts the longer timer for the question screen. */
 
 function startTimer () {
 	intervalID = setInterval(count,1000);
 }
+
+/* Function that ends the timer. */
 
 function clearTimer () {
 	clearInterval(intervalID);
@@ -253,11 +304,32 @@ function clearTimer () {
 
 }
 
+/* Function makes the timer work on the answer screen and when the timer runs out it, it moves on
+to the next question. */
+
+function countAnswer () {
+	 gameState.timer--;
+  	var converted = timeConverter(gameState.timer);
+  	if (converted === "00:00") {
+  		refreshBoard (); 
+  		chooseQuestion (); 
+  	}
+
+}
+
+/* Function that makes the timer work on the question screen and when the timer runs out, it moves
+on to the next question even if the user hasn't put in an answer. */
+
 function count() {
 
   gameState.timer--;
   var converted = timeConverter(gameState.timer);
   $(".timer").html(converted);
+  	if (converted === "00:00") {
+  		refreshBoard (); 
+  		chooseQuestion (); 
+  		
+  	}
 
 }
 
